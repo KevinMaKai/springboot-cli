9619ff0cd02ad08d5851c15e6b5c05abfcf4dc4e
KevinMaKai <qq1382031@126.com>
2020-05-11 10:01:40 +0800

    pom

---
+++ springboot-service/src/main/java/com/kevinmakai/springproject/cli/beancopy/CopyTest1.java
@@ -0,0 +1,13 @@
+package com.kevinmakai.springproject.cli.beancopy;
+
+import lombok.Data;
+
+/**
+ * 请填写类的描述
+ *
+ * @author makai5
+ * @date 2020-05-09 16:58
+ */
+@Data
+public class CopyTest1 {
+}
---
+++ springboot-service/src/main/java/com/kevinmakai/springproject/cli/beancopy/CopyTest2.java
@@ -0,0 +1,10 @@
+package com.kevinmakai.springproject.cli.beancopy;
+
+/**
+ * 请填写类的描述
+ *
+ * @author makai5
+ * @date 2020-05-09 16:59
+ */
+public class CopyTest2 {
+}
